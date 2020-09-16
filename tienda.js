984395522a24fa351f8f8ea22b2a8fdfb6116e77
Elsa-dev <beadearg@hotmail.com>
2020-09-16 19:43:36 -0300

    estilado de botones modal compra

--- tienda.js
+++ tienda.js
@@ -1,50 +1,64 @@
-const inputNombre = document.querySelector('.filtro')
-console.log(inputNombre)
-const botonLimpiar = document.getElementById(`button`)
-console.log(botonLimpiar)
-const checkboxes = document.querySelectorAll(`.review-filter`)
-console.log(checkboxes)
-const filtroRating = document.getElementsByClassName(`review-filter`)
-console.log(filtroRating)
+const filtroNombre = document.querySelector('#filtro')
+
 const tarjetas = document.getElementsByClassName(`producto`)
-console.log(tarjetas)
+
+const botonLimpiar = document.getElementById(`button`)
+
+const checkboxes = document.querySelectorAll(`.review-filter`)
+
+const filtroRating = document.getElementsByClassName(`review-filter`)
+
+
+// filtroNombre.oninput = ( ) => {
+//     for(let tarjeta of tarjetas) {
+//         const titulo = tarjeta.dataset.nombre;
+//         const busqueda = filtroNombre.value;
+//         if(titulo.includes(busqueda)) {
+//             tarjeta.classList.remove("hidden");
+//         }
+//         else {
+//             tarjeta.classList.add("hidden");
+//         }
+//     }
+// }
 
 
 
-for (let checkbox of filtroRating) {
-    checkbox.onclick = () => {
-        filtrarTarjetas();
-    };
-}
 
-const haycheckboxSeleccionado = () => {
-    for (let checkbox of filtroRating) {
-        if (checkbox.checked) {
-            return true
-        }
-    }
-};
+// for (let checkbox of filtroRating) {
+//     checkbox.onclick = () => {
+//         filtrarTarjetas();
+//     };
+// }
 
-const coincidenCheckboxYtarjeta = (tarjeta) => {
-    const rating= tarjeta.dataset.rating;
-    for(let checkbox of filtroRating){
-        if(checkbox.value === rating && checkbox.checked){
-            return true;
-        }
-    }
-};
+// const haycheckboxSeleccionado = () => {
+//     for (let checkbox of filtroRating) {
+//         if (checkbox.checked) {
+//             return true
+//         }
+//     }
+// };
 
-const filtrarTarjetas = () => {
-    for(let tarjeta of tarjetas){
-        tarjeta.classList.add(`hidden`);
-        if(haycheckboxSeleccionado()) {
-            if(coincidenCheckboxYtarjeta(tarjeta)) {
-                tarjeta.classList.remove(`hidden`);
-            }
-        }
-        else{ tarjeta.classList.remove(`hidden`)}
-    }
-};
+// const coincidenCheckboxYtarjeta = (tarjeta) => {
+//     const rating= tarjeta.dataset.rating;
+//     for(let checkbox of filtroRating){
+//         if(checkbox.value === rating && checkbox.checked){
+//             return true;
+//         }
+//     }
+// };
+
+// const filtrarTarjetas = () => {
+//     for(let tarjeta of tarjetas){
+//         tarjeta.classList.add(`hidden`);
+//         if(haycheckboxSeleccionado()) {
+//             if(coincidenCheckboxYtarjeta(tarjeta)) {
+//                 tarjeta.classList.remove(`hidden`);
+//             }
+//         }
+//         else{ tarjeta.classList.remove(`hidden`)}
+//     }
+// };
 
 
 // BOTON DE LIMPIAR INPUT Y CHECKBOXXES 
@@ -54,7 +68,7 @@ botonLimpiar.onclick = () => {
 }
 
 limpiarSeleccionesUsuario = () => {
-    inputNombre.value = ""
+    filtroNombre.value = ""
     for (let checkbox of checkboxes) {
         checkbox.checked = false
     }
